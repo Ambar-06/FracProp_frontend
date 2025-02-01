b5d7eb326fbb91fe31b46b6ff6d10cc54465f73e
Ambar Rizvi <brannstrom9911@gmail.com>
2025-02-01 17:06:02 +0530

    fixed auth context

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,4 +1,4 @@
-'use client'
+'use client';
 
 import { createContext, useState, useEffect, useContext } from "react";
 import { useRouter, usePathname } from 'next/navigation';
@@ -8,37 +8,49 @@ const AuthContext = createContext(null);
 export const AuthProvider = ({ children }) => {
   const router = useRouter();
   const pathname = usePathname();
-  
-  const [user, setUser] = useState(() => localStorage.getItem("user"));
-  const [token, setToken] = useState(() => localStorage.getItem("token"));
-  
-  useEffect(() => {
-    const storedUser = localStorage.getItem("user");
-    const storedToken = localStorage.getItem("token");
 
-    if (storedUser && storedToken) {
-      setUser(JSON.parse(storedUser));
-      setToken(storedToken);
-    } else if (!storedToken && pathname !== "/login" && pathname !== "/signup") {
-      router.push('/login');
+  // Initialize state as null (avoid SSR issue)
+  const [user, setUser] = useState(null);
+  const [token, setToken] = useState(null);
+  const [loading, setLoading] = useState(true); 
+
+  useEffect(() => {
+      const storedUser = localStorage.getItem("user");
+      const storedToken = localStorage.getItem("token");
+
+      if (storedUser && storedToken) {
+        setUser(JSON.parse(storedUser));
+        setToken(storedToken);
+      } else if (!storedToken && pathname !== "/login" && pathname !== "/signup") {
+        router.push('/login');
     }
+    setLoading(false); 
   }, [pathname, router]);
 
   const login = (data) => {
     setUser(data);
     setToken(data.token);
-    localStorage.setItem("user", JSON.stringify(data));
-    localStorage.setItem("token", data.token);
+
+      localStorage.setItem("user", JSON.stringify(data));
+      localStorage.setItem("token", data.token);
+
+
     router.push('/dashboard');  // Redirect to dashboard after login
   };
 
   const logout = () => {
     setUser(null);
     setToken(null);
-    localStorage.removeItem("user");
-    localStorage.removeItem("token");
+
+      localStorage.removeItem("user");
+      localStorage.removeItem("token");
+
     router.push("/login");
   };
+
+  if (loading) {
+    return null; // You can show a loader if you want to wait for the context to be set
+  }
 
   return (
     <AuthContext.Provider value={{ user, token, login, logout }}>
