650f9c0b90f278cef2225954818f1185775b93d9
Ambar Rizvi <brannstrom9911@gmail.com>
2025-02-20 01:12:43 +0530

    fix config

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,12 @@ const nextConfig: NextConfig = {
         port: '8000',
         pathname: '/media/uploads/**',
       },
+      {
+        protocol: 'http',
+        hostname: '127.0.0.1',
+        port: '8000',
+        pathname: '/media/property_images/**',
+      },
     ],
   },
 };
